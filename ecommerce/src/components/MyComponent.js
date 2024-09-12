9570c7e43248b0cd97ab23717213e499bbf83e94
Pinkygs <pinkygs364@gmail.com>
2024-09-12 13:04:45 +0530

    commit all

---
+++ ecommerce/src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MyComponent = () => {
+  return (
+    <div>
+        <h1>Hi i am first react component</h1>
+        <p>this is very 1st react component</p>
+    </div>
+  )
+}
---
+++ ecommerce/src/components/MyComponent2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MyComponent2 = () => {
+  return (
+    <div>
+        <h1>cute cat</h1>
+        <img src = "https://i.ytimg.com/vi/SQJrYw1QvSQ/maxresdefault.jpg"></img>
+    </div>
+  )
+}
